3ade5d1dc3b68794b7649a5a344945f666f9bb18
estefani.rodriguez@escola.pr.gov.br <estefani.rodriguez@escola.pr.gov.br>
2024-09-12 08:56:58 -0300

    editei java script

--- script/aluno.js
+++ script/aluno.js
@@ -1,47 +1,195 @@
 function salvar() {
-    console.log("chegou no javascript");
-    function salvar() {
-        const nome_aluno = document.getElementById(nome_aluno);
-       
-      
-        var headers = new Headers();    
-        headers.append("Content-Type", "application/json");
-        headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
-      
-        fetch('localhost:8080/aluno/inserir',{
-      
-          method: "POST",
-          mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
-          cache: "no-cache",
-         
-          // Convertendo o objeto JavaScript para JSON
-          // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
-          body: JSON.stringify({ nome: nome_aluno }),
-      
-          headers: headers
-      
-          //Aqui inicia função then
-        }).then(response => {
-      
-          if(response.ok) {
-      
-            //Esta linha imprime a mensagem no concole
-            console.log('Foi no servidor e voltou');
-      
-            //Esta linha carrega a página sucesso
-            window.location.href = 'sucesso.html'    
-          } else {
-            //Esta linha imprime a mensagem no console
-            console.log('Aconteceu algo que não foi possivel salvar');
-      
-            //Esta linha imprime a mensagem de erro
-            throw new Error('Erro ao tentar salvar');
-          }
-      
-        })
-        //Aqui será executado caso a then não seja chamado
-        .catch(error => console.error('Erro!:', error));
+  const nome_aluno = document.getElementById(nome_aluno);
+    
+  
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+  
+  fetch('localhost:8080/aluno/inserir',{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+    
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+    //Aqui inicia função then
+   }).then(response => {
+
+      if(response.ok) {
+
+    //Esta linha imprime a mensagem no concole
+    console.log('Foi no servidor e voltou');
+
+    //Esta linha carrega a página sucesso
+    window.location.href = 'sucesso.html'    
+    } else {
+    //Esta linha imprime a mensagem no console
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+    //Esta linha imprime a mensagem de erro
+    throw new Error('Erro ao tentar salvar');
+    }
+
+   })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
          
       
-      }
 }
+    
+
+
+
+
+        
+    
+function consultar() {
+  const nome_aluno = document.getElementById(nome_aluno);
+        
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+
+  fetch('localhost:8080/aluno/inserir',{
+
+    method: "POST",
+    mode: "cors", 
+    cache: "no-cache",
+    
+    
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+  
+   }).then(response => {
+
+    if(response.ok) {
+
+        
+    console.log('Foi no servidor e voltou');
+
+    
+    window.location.href = 'sucesso.html'    
+  } else {
+    
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+    
+    throw new Error('Erro ao tentar salvar');
+  }
+
+   })
+
+  .catch(error => console.error('Erro!:', error));
+    
+      
+}
+        
+      
+
+
+
+
+  
+function alterar() {
+  const nome_aluno = document.getElementById(nome_aluno);
+  
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+
+  fetch('localhost:8080/aluno/inserir',{
+
+    method: "POST",
+    mode: "cors", 
+    cache: "no-cache",
+      
+      
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+                  
+   }).then(response => {
+
+    if(response.ok) {
+
+      
+    console.log('Foi no servidor e voltou');
+
+      
+    window.location.href = 'sucesso.html'    
+    } else {
+      
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+      
+    throw new Error('Erro ao tentar salvar');
+    }
+   })
+
+  .catch(error => console.error('Erro!:', error));
+  
+            
+}
+        
+        
+        
+
+
+
+        
+function apagar() {
+  const nome_aluno = document.getElementById(nome_aluno);
+  
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+
+  fetch('localhost:8080/aluno/inserir',{
+
+    method: "POST",
+    mode: "cors", 
+    cache: "no-cache",
+                  
+                    
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+  
+   }).then(response => {
+
+    if(response.ok) {
+
+      
+    console.log('Foi no servidor e voltou');
+
+      
+    window.location.href = 'sucesso.html'    
+    } else {
+      
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+      
+    throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+
+ .catch(error => console.error('Erro!:', error));
+                 
+              
+}
+        
+      
