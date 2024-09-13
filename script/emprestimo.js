341a90ce1d27cb4829a19b122a6444a2b54f517a
rafa244 <rafael.nunes.oliveira@escpla.pr.gov.br>
2024-09-13 11:46:00 -0300

    mudanca js

--- script/emprestimo.js
+++ script/emprestimo.js
@@ -1,28 +1,238 @@
-var meuFormulario = document.getElementById("cadastro_professor");
-meuFormulario.addEventListener("submit", function(event) {
-  event.preventDefault();
-  cadastrar_professor();
+document.addEventListener("DOMContentLoaded", function() {
+  carregarComboLocal();
 });
 
-function capturarPrimeiroNumeroInteiro() {
-  const primeiro_numero_inteiro = Number(document.getElementById("nome_prof").value);
-  return primeiro_numero_inteiro;
+
+
+
+
+function salvar() {
+  const nome_emprestimo = document.getElementById(nome_aluno);
+    
+  
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+  
+  fetch('localhost:8080/emprestimo/inserir',{
+
+    method: "GET",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+    
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+    //Aqui inicia função then
+   }).then(response => {
+
+      if(response.ok) {
+
+    //Esta linha imprime a mensagem no concole
+    console.log('Foi no servidor e voltou');
+
+    //Esta linha carrega a página sucesso
+    window.location.href = 'sucesso.html'    
+    } else {
+    //Esta linha imprime a mensagem no console
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+    //Esta linha imprime a mensagem de erro
+    throw new Error('Erro ao tentar salvar');
+    }
+
+   })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+         
+      
 }
+          
 
-function capturarSegundoNumeroInteiro() {
- const segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
- return segundo_numero_inteiro;
+
+    
+function consultar() {
+  const nome_emprestimo = document.getElementById(nome_aluno);
+        
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+
+  fetch('localhost:8080/aluno/inserir',{
+
+    method: "GET",
+    mode: "cors", 
+    cache: "no-cache",
+    
+                                                           
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+  
+   }).then(response => {
+
+    if(response.ok) {
+
+        
+    console.log('Foi no servidor e voltou');
+
+    
+    window.location.href = 'sucesso.html'    
+  } else {
+    
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+    
+    throw new Error('Erro ao tentar salvar');
+  }
+
+   })
+
+  .catch(error => console.error('Erro!:', error));
+    
+      
 }
+        
+      
 
 
-function somar() {
-  let primeiro_numero_inteiro = capturarPrimeiroNumeroInteiro();
-  let segundo_numero_inteiro = capturarSegundoNumeroInteiro();
-  let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
-  imprimirResultadoNaTela(resultado_soma);
+
+
+  
+function alterar() {
+  const nome_emprestimo = document.getElementById(nome_aluno);
+  
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+
+  fetch('localhost:8080/emprestimo/inserir',{
+
+    method: "GET",
+    mode: "cors", 
+    cache: "no-cache",
+      
+      
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+                  
+   }).then(response => {
+
+    if(response.ok) {
+
+      
+    console.log('Foi no servidor e voltou');
+
+      
+    window.location.href = 'sucesso.html'    
+    } else {
+      
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+      
+    throw new Error('Erro ao tentar salvar');
+    }
+   })
+
+  .catch(error => console.error('Erro!:', error));
+  
+            
 }
+        
+        
+        
 
 
-function imprimirResultadoNaTela(resultado_soma) {
-  document.getElementById("resultado_soma").value = resultado_soma;
+
+        
+function apagar() {
+  const nome_emprestimo = document.getElementById(nome_aluno);
+  
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+
+  fetch('localhost:8080/emprestimo/inserir',{
+
+    method: "GET",
+    mode: "cors", 
+    cache: "no-cache",
+                  
+                    
+    body: JSON.stringify({ nome: nome_aluno }),
+
+    headers: headers
+
+  
+   }).then(response => {
+
+    if(response.ok) {
+
+      
+    console.log('Foi no servidor e voltou');
+
+      
+    window.location.href = 'sucesso.html'    
+    } else {
+      
+    console.log('Aconteceu algo que não foi possivel salvar');
+
+      
+    throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+
+ .catch(error => console.error('Erro!:', error));
+                 
+              
+}
+        
+      
+
+
+
+
+function carregarComboLocal() {
+ 
+  console.log('Carregou a página e chamou a função');
+
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('http://127.0.0.1:8080/local/findAll' ,{
+
+    method: "GET",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+
+    headers: headers
+
+    
+  }).then(response => response.json())
+  .then(data => {
+      const comboBox = document.getElementById('locais');
+      data.forEach(local => {
+          const option = document.createElement('option');
+          option.value = local.id;
+          option.textContent = local.nome;
+          comboBox.appendChild(option);
+      });
+  })
+  .catch(error => console.error('Erro ao carregar locais:', error));
+   
+
 }
